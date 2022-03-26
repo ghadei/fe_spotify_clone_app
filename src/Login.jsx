6c4336084a3886d534ef6a4e9bea307dd87951f6
Chandrakanta <ch.ghadei@gmail.com>
2022-03-26 14:54:05 +0530

    update;

---
+++ src/Login.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+// require('dotenv').config()
+
+const auth_url = process.env.REACT_APP_AUTHURL
+
+const Login = () => {
+
+    return (
+        <>
+
+
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">Hello there</h1>
+                        <p className="py-6">This is a clone application of spotify developed by Chandra (ch.ghadei@gmail.com). In order to use it, you need a spotify premium profile.</p>
+                        <button className="btn btn-primary">
+                            <a href={auth_url}>Login</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </>
+    )
+}
+
+
+export default Login;
